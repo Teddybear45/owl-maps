ee736080c9abd21e880a26f6b22e017771f07d82
teddybear45 <tedguan2005@gmail.com>
2020-08-27 14:51:29 -0700

    has map, bad formatting

--- JS/Map.js
+++
@@ -1,7 +0,0 @@
-var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYmVhcno0bHlmIiwiYSI6ImNrZWNhMGFrZjAyNjMyenA5NzI5emp6c3QifQ.TAZD8AYKlTOX0JPvaXTROg';
-var map = new mapboxgl.Map({
-  container: 'YOUR_CONTAINER_ELEMENT_ID',
-  style: 'mapbox://styles/mapbox/streets-v11'
-});
